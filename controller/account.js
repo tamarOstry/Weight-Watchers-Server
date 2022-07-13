47c7114184fde98052fe3307a96cd39ed6ee41c5
Ester-Tofik <325202349@mby.co.il>
2022-07-13 13:08:21 +0300

    Modification of functions due to the connection of the server and the client

--- controller/account.js
+++ controller/account.js
@@ -2,7 +2,8 @@ const accountService = require('../services/account.js')
 
 exports.getByEmailPassword = async function (req, res, next) {
   try {
-    const { email, password } = req.body;
+    const { email } =req.body ;
+    const { password } =req.body ;
     const user = await accountService.getByEmailPassword(email, password);
     res.send(user);
   }
--- services/account.js
+++ services/account.js
@@ -4,8 +4,17 @@ const getData = async () => fs.readFile('./users.json').then(data => JSON.parse(
 
 module.exports.getByEmailPassword = async (email, password) => {
     const data = await getData();
-    const users = data.users || [];
-    const user = users.find(user => user.email == email && user.password == password);
-    console.log(user);
-    return user;
+    const users = data.users;
+    const manager = data.manager;
+    if (users.length > 0) {
+        if (manager.email === email && manager.password == password) {
+            return manager;
+        }
+        const user = users.find(user => user.email == email && user.password == password);
+        console.log(user);
+        return JSON.stringify(user);
+
+    } else {
+        console.log('No users found');
+    }
 }
